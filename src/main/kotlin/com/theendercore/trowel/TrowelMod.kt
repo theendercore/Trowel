cd7453f5cd5c35b590465433fe646a480fa7a656
theendercore <theendercore@gmail.com>
2023-06-10 18:05:07 +0300

    1.19.2 port

--- src/main/kotlin/com/theendercore/trowel/TrowelMod.kt
+++ src/main/kotlin/com/theendercore/trowel/TrowelMod.kt
@@ -1,15 +1,11 @@
 package com.theendercore.trowel
 
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.ModifyEntries
 import net.minecraft.item.Item
-import net.minecraft.item.ItemGroups
 import net.minecraft.item.Items
-import net.minecraft.registry.Registries
 import org.slf4j.LoggerFactory
-import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry;
 
 object TrowelMod : ModInitializer {
     const val MODID = "trowel"
@@ -18,7 +14,7 @@ object TrowelMod : ModInitializer {
     override fun onInitialize() {
         LOGGER.info("Seizing the means of block placement!")
 
-        Registry.register(Registries.ITEM, Identifier(MODID, MODID), TROWEL)
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModifyEntries { it.addAfter(Items.SHEARS,TROWEL) })
+        Registry.register(Registry.ITEM, Identifier(MODID, MODID), TROWEL)
+        // ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModifyEntries { it.addAfter(Items.SHEARS,TROWEL) })
     }
 }
